8fe2fdc1485853aaa6194696de2afc95e155dce6
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2024-03-09 08:31:12 +0000

    Add files via upload

---
+++ aptitude.py
@@ -0,0 +1,22 @@
+
+n =int(input("enter the number line: "))
+
+for i in range(1,n+1):
+   
+    for j in range(n-i):
+       
+        print(" ",end =" ")
+    for j in range(i):
+        
+        print(i-j,end =" ")
+    for j in range(2,i+1):
+        print(j,end =" ")
+    print()
+
+        
+    
+
+
+
+ 
+
